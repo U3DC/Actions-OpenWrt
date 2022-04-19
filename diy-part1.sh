43e660a93a1511cef363396f57e7df3961a0fa08
NASA\Mark <tuqibiao@gmail.com>
2022-04-19 10:58:20 +0800

    修改feed源

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
-echo 'src-git ssrplus https://github.com/maxlicheng/luci-app-ssr-plus' >>feeds.conf.default
+#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+#echo 'src-git ssrplus https://github.com/maxlicheng/luci-app-ssr-plus' >>feeds.conf.default
+
+echo 'src-git package https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git package https://github.com/kenzok8/small.git' >>feeds.conf.default
