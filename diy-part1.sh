dcb9635a7375de697c968be562657975b74ac766
NASA\Mark <tuqibiao@gmail.com>
2022-04-19 10:31:33 +0800

    更新ssr，openclash

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,5 +14,7 @@
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git ssrplus https://github.com/maxlicheng/luci-app-ssr-plus' >>feeds.conf.default
