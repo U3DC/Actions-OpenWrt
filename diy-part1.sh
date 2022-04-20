bfa726a19bf9c4ea58f3623680640a32520c1f7e
NASA\Mark <tuqibiao@gmail.com>
2022-04-20 09:05:02 +0800

    更新源。

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 
-sed -i '$a src-git kenzo https://github.com/U3DC/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
